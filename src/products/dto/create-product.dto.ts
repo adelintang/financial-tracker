c35e5d6d0073fc2c1d3b1f07e52447754870b49a
adelintang <adelintang157@gmail.com>
2024-11-23 02:54:58 +0700

    pref(product): validation request

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,18 @@
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   desc: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
   qty: number;
 }
