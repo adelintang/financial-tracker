e52b9dc68bd8849be33dc0edb434d9ea53cd3a5f
adelintang <adelintang157@gmail.com>
2024-11-23 00:31:47 +0700

    build: setup env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
