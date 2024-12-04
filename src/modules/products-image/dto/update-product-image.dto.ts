688e661fa68645cc267831efc0cfce955c7acfd7
adelintang <adelintang157@gmail.com>
2024-12-05 01:33:39 +0700

    pref(product-image): omit type product_id and public_id in update dto

--- src/modules/products-image/dto/update-product-image.dto.ts
+++ src/modules/products-image/dto/update-product-image.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, OmitType } from '@nestjs/mapped-types';
 import { CreateProductImageDto } from './create-product-image.dto';
 
-export class UpdateProductImageDto extends PartialType(CreateProductImageDto) {}
+export class UpdateProductImageDto extends PartialType(
+  OmitType(CreateProductImageDto, ['product_id', 'public_id'] as const),
+) {}
