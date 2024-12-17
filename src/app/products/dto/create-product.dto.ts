d77bdcb1ee5de6e0fe71ef0f4b6583503f8c2c94
adelintang <adelintang157@gmail.com>
2024-12-17 13:57:37 +0700

    feat(products-swagger): example request body in create and update product

--- src/app/products/dto/create-product.dto.ts
+++ src/app/products/dto/create-product.dto.ts
@@ -3,26 +3,41 @@ import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Name of product',
+    example: 'iPhone 16 Pro Max',
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Description of product',
+    example: 'Iphone terbaru keluaran terbaru seri 16',
+  })
   desc: string;
 
   @IsNumber()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Price of product',
+    example: 20000000,
+  })
   price: number;
 
   @IsNumber()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Qty of product',
+    example: 50,
+  })
   qty: number;
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty()
+  @ApiProperty({
+    description: 'id of user',
+    example: '5c4394f7-d3e3-48d1-8a65-e4324fa7141e',
+  })
   user_id: string;
 }
