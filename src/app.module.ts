2ee5b03fb79cbef86ffb0826b4de8724f3ce2a58
adelintang <adelintang157@gmail.com>
2025-02-05 20:42:11 +0700

    build: delete product and product image

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ProductsModule } from './app/products/products.module';
 import { UsersModule } from './app/users/users.module';
 import { AuthModule } from './app/auth/auth.module';
-import { ProductsImageModule } from './app/products-image/products-image.module';
 import { CommonModule } from './common/common.module';
 import { HttpLoggerMiddleware } from './common/middlewares/http-logger.middleware';
 
 @Module({
-  imports: [
-    CommonModule,
-    ProductsModule,
-    UsersModule,
-    AuthModule,
-    ProductsImageModule,
-  ],
+  imports: [CommonModule, UsersModule, AuthModule],
   controllers: [],
   providers: [],
 })
