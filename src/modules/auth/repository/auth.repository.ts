1b6cf91112a8255c45c58f1df0ca8a3326c0a670
adelintang <adelintang157@gmail.com>
2025-02-06 01:58:26 +0700

    feat(auth): repository

--- src/modules/auth/repository/auth.repository.ts
+++ src/modules/auth/repository/auth.repository.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Role } from '@prisma/client';
 import { PrismaService } from '../../../common/providers/prisma/prisma.service';
+import { RegisterAuthDto } from '../dto/register-auth.dto';
 
 @Injectable()
 export class AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  async register(registerAuthDto: any) {
-    return this.prisma.user.create(registerAuthDto);
+  async register(registerAuthDto: RegisterAuthDto) {
+    return this.prisma.user.create({
+      data: { ...registerAuthDto, role: Role.USER },
+    });
   }
 
-  async getUserByUsername(email: string) {
+  async getUserByEmail(email: string) {
     return this.prisma.user.findUnique({
       where: {
         email,
