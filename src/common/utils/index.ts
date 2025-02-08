94bd560157c7d0be6c176c932fdcc866c308d939
adelintang <adelintang157@gmail.com>
2025-02-08 23:19:56 +0700

    feat(utils): generate otp

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -29,4 +29,12 @@ export class Utils {
       totalData: total,
     };
   }
+
+  static GenerateOtp() {
+    let result = '';
+    for (let i = 0; i < 4; i++) {
+      result += Math.floor(Math.random() * 10);
+    }
+    return result;
+  }
 }
