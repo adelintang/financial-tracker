5754cc460bd5e560d0980d051f5f20f249d00e18
adelintang <adelintang157@gmail.com>
2025-02-11 20:06:58 +0700

    pref(user): interface and mapper

--- src/modules/users/mapper/user.mapper.ts
+++ src/modules/users/mapper/user.mapper.ts
@@ -1,7 +1,15 @@
 import { User } from '@prisma/client';
+import { IUser } from '../models/user.interface';
 
-export const usersMapper = (users) => {
+export const usersMapper = (users: User[]): IUser[] => {
   return users.map((user) => userMapper(user));
 };
 
-export const userMapper = (user) => {};
+export const userMapper = (user: User): IUser => {
+  return {
+    id: user.id,
+    email: user.email,
+    name: user.name,
+    currency: user.currency,
+  };
+};
--- src/modules/users/models/user.interface.ts
+++ src/modules/users/models/user.interface.ts
@@ -1,1 +1,6 @@
-
+export interface IUser {
+  id: string;
+  email: string;
+  name: string;
+  currency: string;
+}
