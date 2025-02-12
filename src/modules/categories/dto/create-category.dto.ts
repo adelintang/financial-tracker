94468bd8913adef6ba5e2fc01a3a804ef58ada7d
adelintang <adelintang157@gmail.com>
2025-02-12 22:28:41 +0700

    feat(category): create category dto

---
+++ src/modules/categories/dto/create-category.dto.ts
@@ -0,0 +1,12 @@
+import { TransactionType } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEnum(TransactionType)
+  @IsNotEmpty()
+  type: TransactionType;
+}
