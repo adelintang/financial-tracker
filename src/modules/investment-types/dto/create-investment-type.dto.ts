5768d11f2c397ad0d8cde44bf67b1a8964922e18
adelintang <adelintang157@gmail.com>
2025-02-12 23:39:11 +0700

    pref(swagger): create investment type dto

--- src/modules/investment-types/dto/create-investment-type.dto.ts
+++ src/modules/investment-types/dto/create-investment-type.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateInvestmentTypeDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'name of type investment',
+    example: 'Crypto',
+  })
   type: string;
 }
