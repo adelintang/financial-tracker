834a85a9055ce8217a47217b647700355cda4fee
adelintang <adelintang157@gmail.com>
2025-02-12 23:04:13 +0700

    feat(swagger): create category dto

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,12 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { TransactionType } from '@prisma/client';
 import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'name of category',
+    example: 'Makanan',
+  })
   name: string;
 
   @IsEnum(TransactionType)
   @IsNotEmpty()
+  @ApiProperty({ enum: [TransactionType.EXPENSE, TransactionType.INCOME] })
   type: TransactionType;
 }
