6d7dd771743a041a9e5a22123441d6d94b474b8b
adelintang <adelintang157@gmail.com>
2025-02-16 21:58:15 +0700

    feat(swagger): investment-type

---
+++ src/modules/investment-types/swagger/index.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { GeneralResponseSwagger } from '../../../common/swagger';
+import { InvestmentTypeResponse } from '../models/investment-type.response';
+import { Const } from 'src/common/constans';
+
+export class CreateInvestmentTypeResponseSwagger extends GeneralResponseSwagger<InvestmentTypeResponse> {
+  @ApiProperty({ example: Const.MESSAGE.SUCCESS.CREATED.INVESTMENT_TYPE })
+  message: string;
+
+  @ApiProperty({ type: InvestmentTypeResponse })
+  data: InvestmentTypeResponse;
+}
+
+export class InvestmentTypesResponseSwagger extends GeneralResponseSwagger<
+  InvestmentTypeResponse[]
+> {
+  @ApiProperty({ example: Const.MESSAGE.SUCCESS.GET.INVESTMENT_TYPES })
+  message: string;
+
+  @ApiProperty({ type: [InvestmentTypeResponse] })
+  data: InvestmentTypeResponse[];
+}
