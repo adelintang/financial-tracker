a807b245cb757147ae036e3d3b6205639127d456
adelintang <adelintang157@gmail.com>
2025-02-16 21:23:40 +0700

    feat(category): apply mapper to service

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -3,16 +3,25 @@ import { TransactionType } from '@prisma/client';
 import { CategoriesRepository } from './repository/categories.repository';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { QueryParams } from '../../types';
+import { CategoryResonse } from './models/category.response';
+import { categoriesMapper, categoryMapper } from './mapper/categories.mapper';
 
 @Injectable()
 export class CategoriesService {
   constructor(private readonly categoriesRepository: CategoriesRepository) {}
 
-  async createCategory(createCategoryDto: CreateCategoryDto) {
-    return this.categoriesRepository.createCategory(createCategoryDto);
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+  ): Promise<CategoryResonse> {
+    const createCategory =
+      await this.categoriesRepository.createCategory(createCategoryDto);
+    return categoryMapper(createCategory);
   }
 
-  async getCategories(query: QueryParams & { type: TransactionType }) {
-    return this.categoriesRepository.getCategories(query);
+  async getCategories(
+    query: QueryParams & { type: TransactionType },
+  ): Promise<CategoryResonse[]> {
+    const categories = await this.categoriesRepository.getCategories(query);
+    return categoriesMapper(categories);
   }
 }
